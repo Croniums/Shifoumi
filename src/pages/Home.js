93fb7b3350dc577edf7fd422174074dc278a80e8
Croniums <valentin.thomas@efrei.net>
2022-04-05 09:59:57 +0200

    correction

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+      <>
+        <div className="App">
+        <div className="App-header">
+          <h1>Home</h1>
+        </div>
+      </div>
+      </>
+    );
+  }
+
+export default Home;
